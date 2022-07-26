2a945c8937b7088d83168ca9bf7df4abf08dca53
Kurt <4968kms@gmail.com>
2022-07-26 19:29:10 +0100

    test_final

--- Now_time_jimin.py
+++ Now_time_jimin.py
@@ -3,101 +3,76 @@ import time
 import datetime
 import sys
 import zoneinfo
+import pytz
+import asyncio
+import schedule
 from apscheduler.triggers.cron import CronTrigger
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord.ext import commands
+from datetime import datetime
+from datetime import date
+from pytz import timezone
 
+datetime.now(timezone('Asia/Seoul'))
+time = datetime.now(timezone('Asia/Seoul'))
 bot = commands.Bot(command_prefix="./")
-
-@bot.command
-async def func():
-    #await bot.wait_until_ready()
-    await ctx.send("지짐시")
-@bot.event
-async def now_time():
-    await client.wait.until_ready()
-    channel = client.get_channel(915862213074493540)
-    msg_send = False
-
+def time_module():
+    print("time module in use")
     while True:
-        if time.hour == 01 and time.minute == 11:
-            await channel.send("지짐시")
-            msg_sent = True
-        else:
-            msg_sent = True
+        current_time = datetime.now(timezone('Asia/Seoul')).strftime("%H:%M")
+        if current_time == "01:11":
+            print("time module ended")
+            break
 
-        if time.hour == 02 and time.minute == 22:
-            await channel.send("지짐시")
-            msg_sent = True
-        else:
-            msg_sent = True
+        if current_time == "02:22":
+            print("time module ended")
+            break
         
-        if time.hour == 02 and time.minute == 06:
-            await channel.send("지짐시")
-            msg_sent = True
-        else:
-            msg_sent = True
-        if time.hour == 03 and time.minute == 33:
-            await channel.send("지짐시")
-            msg_sent = True
-        else:
-            msg_sent = True
+        if current_time == "03:33":
+            print("time module ended")
+            break
+        
+        if current_time == "04:44":
+            print("time module ended")
+            break
+        
+        if current_time == "05:55":
+            print("time module ended")    
+            break
 
-        if time.hour == 04  and time.minute == 44:
-            await channel.send("지짐시")
-            msg_sent = True
-        else:
-            msg_sent = True
+        if current_time == "13:11":
+            print("time module ended")
+            break
 
-        if time.hour == 05 and time.minute == 55:
-            await channel.send("지짐시")
-            msg_sent = True
-        else:
-            msg_sent = True
+        if current_time == "14:22":
+            print("time module ended")
+            break
 
-        if time.hour == 13  and time.minute == 11:
-            await channel.send("지짐시")
-            msg_sent = True
-        else:
-            msg_sent = True
+        if current_time == "15:33":
+            print("time module ended")
+            break
 
-        if time.hour == 14 and time.minute == 22:
-            await channel.send("지짐시")
-            msg_sent = True
-        else:
-            msg_sent = True
+        if current_time == "16:44":
+            print("time module ended")
+            break
 
-        if time.hour == 15  and time.minute == 33:
-            await channel.send("지짐시")
-            msg_sent = True
-        else:
-            msg_sent = True
+        if current_time == "17:55":
+            print("time module ended")
+            break
 
-        if time.hour == 16 and time.minute == 44:
-            await channel.send("지짐시")
-            msg_sent = True
-        else:
-            msg_sent = True
+time_module()
 
-        if time.hour == 17 and time.minute == 55:
-            await channel.send("지짐시")
-            msg_sent = True
-        else:
-            msg_sent = True
-#@bot.event
-#async def on_ready():
-    #scheduler = AsyncIOScheduler()
-
-    #scheduler.add_job(func, CronTrigger(hour="01, 02, 03, 04, 05, 13, 14, 15, 16, 17", minute="11, 22, 33, 44, 55", second="0"))
-
-    #scheduler.start()
+@bot.event
+async def on_ready():
+    channel = bot.get_channel(915862213074493543)
+    await channel.send(f"지짐시, {386099935163973634}")
 @bot.command()
 async def 소환(ctx):
     await ctx.send('쥐미나 @')
 
 @bot.command()
 async def 지짐시(ctx):
-    nowtime = datetime.datetime.now()
+    nowtime = datetime.now(timezone('Asia/Seoul'))
     await ctx.send('쥐금 시간')
     await ctx.send(nowtime)
 
