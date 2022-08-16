0b3148dedae988a00f447db87829c000e8ae6a41
Kurt Kim <4968kms@gmail.com>
2022-08-16 14:42:19 +0900

    Ver.5

--- Now_time_jimin.py
+++ Now_time_jimin.py
@@ -1,4 +1,3 @@
-
 import discord
 import time
 import datetime
@@ -26,101 +25,101 @@ async def on_ready():
     print(bot.user.name)
     print('로그인에 성공했습니다')
     await bot.change_presence(status=discord.Status.online, activity=None)
-    await cal_time()
-    set_next_time()
+    await cal_time()    
 
 def set_next_time():
     global next_time
     while True:
-        if current_time > "01:11" and current_time <= "02:22":
+        if  datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") > "01:11" and datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") <= "02:22":
             next_time = "02:22"
             break
 
-        if current_time > "02:22" and current_time <= "03:33":
+        if  datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") > "02:22" and datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") <= "03:33":
             next_time = "03:33"
             break
 
-        if current_time > "03:33" and current_time <= "04:44":
+        if  datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") > "03:33" and datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") <= "04:44":
             next_time = "04:44"
             break
 
-        if current_time > "04:44" and current_time <= "05:55":
+        if  datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") > "04:44" and datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") <= "05:55":
             next_time = "05:55"
             break
 
-        if current_time > "05:55" and current_time <= "10:10":
+        if  datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") > "05:55" and datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") <= "10:10":
             next_time = "10:10"
             break
 
-        if current_time > "10:10" and current_time <= "11:11":
+        if  datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") > "10:10" and datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") <= "11:11":
             next_time = "11:11"
             break
 
-        if current_time > "11:11" and current_time <= "12:12":
+        if datetime.now(timezone('Asia/Seoul')).strftime("%H:%M")  > "11:11" and datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") <= "12:12":
             next_time = "12:12"
             break
 
-        if current_time > "12:12" and current_time <= "13:11":
+        if  datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") > "12:12" and datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") <= "13:11":
             next_time = "13:11"
             break
 
-        if current_time > "13:11" and current_time <= "14:22":
+        if  datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") > "13:11" and datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") <= "14:22":
             next_time = "14:22"
             break
 
-        if current_time > "14:22" and current_time <= "15:33":
+        if  datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") > "14:22" and datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") <= "15:33":
             next_time = "15:33"
             break
 
-        if current_time > "15:33" and current_time <= "16:44":
+        if  datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") > "15:33" and datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") <= "16:44":
             next_time = "16:44"
             break
 
-        if current_time > "16:44" and current_time <= "17:55":
+        if datetime.now(timezone('Asia/Seoul')).strftime("%H:%M")  > "16:44" and datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") <= "17:55":
             next_time = "17:55"
             break
 
-        if current_time > "17:55" and current_time <= "22:10":
+        if datetime.now(timezone('Asia/Seoul')).strftime("%H:%M")  > "17:55" and datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") <= "22:10":
             next_time = "22:10"
             break
 
-        if current_time > "22:10" and current_time <= "23:11":
+        if  datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") > "22:10" and datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") <= "23:11":
             next_time = "23:11"
             break
 
-        if current_time > "23:11":
+        if  datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") > "23:11":
             next_time = "01:11"
             break
 
 @bot.event
 async def now_time_jimin():
-    print(current_time, "지짐시 호출됨")
     channel = bot.get_channel(915862213074493543)
     await channel.send("**<@386099935163973634> 지짐시**")
     await asyncio.sleep(60)
-    await cal_time
+    await cal_time()
 
 async def cal_time():
     global next_time
-    print("arst")
     set_next_time()
     while True:
-        if current_time==next_time:
+        if datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") ==next_time:
             await now_time_jimin()
             break
         else:
-            if current_time!=next_time:
+            if  datetime.now(timezone('Asia/Seoul')).strftime("%H:%M") !=next_time:
                 await asyncio.sleep(30)
                 await cal_time()
                 break
 
 @bot.command()
 async def 지짐시(ctx):
-    print(current_time, "다음 지짐시 호출됨")
     global next_time
+    print(datetime.now(timezone('Asia/Seoul')).strftime("%H:%M"), "지짐시 호출됨")
     print(next_time)
     await ctx.channel.send(f'다음 지짐시는 {next_time}입니다.')
 
+@bot.command()
+async def 진정(ctx):
+    await ctx.channel.send(f"{ctx.message.mentions[0].mention}, {ctx.message.author.mention}(이)가 진정하래")
 
 #@bot.event #지민 진정 기능
 #async def on_message(message):
@@ -128,5 +127,16 @@ async def 지짐시(ctx):
 #        msg = await message.channel.send(f"{message.author.mention} 진정")
 #        await asyncio.sleep(0.5)
 #        await msg.delete()
+
+now = datetime.now()
+time = f"{str(now.year)}년 {str(now.month)}월 {str(now.day)}일 {str(now.hour)}시 {str(now.minute)}분 {str(now.second)}초"
+
+@bot.event
+async def on_message_delete(message):
+    channel = bot.get_channel(1004709248422387722)
+    embed = discord.Embed(title=f"삭제됨", description=f"유저 : {message.author.mention} 채널 : {message.channel.mention}", color=0xFF0000)
+    embed.add_field(name="삭제된 내용", value=f"내용 : {message.content}", inline=False)
+    embed.set_footer(text=f"{message.guild.name} | {time}")
+    await channel.send(embed=embed)
 
 bot.run("MTAwMTUwOTg4Mjg4Njg4MTI4MA.GoEBhL.kDyy0fl7LIU3MMmoEpA8BF8rK7h0Qjd_Qbc7iw")
